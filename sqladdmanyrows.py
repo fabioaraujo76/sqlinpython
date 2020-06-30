bc79e19eb74eb2b4a424ace7f0cb655f23c79894
Fabio <farofastech@gmail.com>
2020-06-30 23:44:36 +0000

    include all different sql operations with python

--- sqladdmanyrows.py
+++ sqladdmanyrows.py
@@ -1,7 +1,7 @@
 import os
 import pymysql
 
-# Get the username from the Cloud9 workspace
+# Get username from Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -10,15 +10,15 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-try:
-    with connection.cursor() as cursor:
-        list_of_names = ['fred', 'Fred']
-        # Prepare a string with same number of placeholders as in list_of_names
-        format_strings = ','.join(['%s'] * len(list_of_names))
-        cursor.execute(
-            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
-            list_of_names)
 
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),
+                ("Jim", 56, "1955-05-09 13:12:45"),
+                ("Fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
         connection.commit()
 finally:
+    # Close the connection, regardless of
     connection.close()
--- sqldeletewherein.py
+++ sqldeletewherein.py
@@ -1,7 +1,7 @@
 import os
 import pymysql
 
-# Get the username from the Cloud9 workspace
+# Get username from Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -10,15 +10,12 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-try:
-    with connection.cursor() as cursor:
-        list_of_names = ['fred', 'Fred']
-        # Prepare a string with same number of placeholders as in list_of_names
-        format_strings = ','.join(['%s'] * len(list_of_names))
-        cursor.execute(
-            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
-            list_of_names)
 
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        cursor.execute("DELETE FROM Friends WHERE name in ('jim','bob')")
         connection.commit()
 finally:
+    # Close the connection, regardless of
     connection.close()
